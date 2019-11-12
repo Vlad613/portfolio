8bb9e1b231316cc680271d557c4e08e4f71459c8
Vlad613 <vladislav613zharnikov@gmail.com>
2019-11-12 18:09:44 +0300

    Updates

---
+++ precache-manifest.9bdeee1e8a33edd7ac0c27fc3490c390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7000c233cc0955f585ed6d848508cd8c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f142b69e285f820afd91",
+    "url": "/portfolio/static/css/main.b5252ecb.chunk.css"
+  },
+  {
+    "revision": "8e212c2b2dac7538af8a",
+    "url": "/portfolio/static/js/2.baea1c18.chunk.js"
+  },
+  {
+    "revision": "f142b69e285f820afd91",
+    "url": "/portfolio/static/js/main.7ef0d446.chunk.js"
+  },
+  {
+    "revision": "fb4ef41b379f7c47d959",
+    "url": "/portfolio/static/js/runtime-main.971f9005.js"
+  }
+]);
