6b92eea6bd6e50d612005854a9def3bf2af7a068
Vlad613 <55757119+Vlad613@users.noreply.github.com>
2020-06-16 13:18:59 +0300

    1.1_after_comments

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -3,30 +3,37 @@ import styles from './Projects.module.css';
 import ItemProjects from "./ItemProjects/ItemProjects";
 
 
-
 function Projects() {
-  return (
-    <div className={styles.projects}>
-      <div className={styles.container}>
+    return (
+        <div className={styles.projects}>
+            <div className={styles.container}>
 
-        <div className={styles.myProjects}>
-           Мои работы
+                <div className={styles.myProjects}>
+                    Мои работы
+                </div>
+
+                <div className={styles.allProjects}>
+
+                    <div className={styles.itemProject}>
+                        <ItemProjects/>
+                    </div>
+
+                    <div className={styles.itemProject}>
+                        <ItemProjects/>
+                    </div>
+
+                    <div className={styles.itemProject}>
+                        <ItemProjects/>
+                    </div>
+
+                    <div className={styles.itemProject}>
+                        <ItemProjects/>
+                    </div>
+
+                </div>
+            </div>
         </div>
-
-        <div className={styles.allProjects}>
-
-          <div className={styles.itemProject}>
-           <ItemProjects/>
-          </div>
-
-            <div className={styles.itemProject}>
-            <ItemProjects/>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Projects;
