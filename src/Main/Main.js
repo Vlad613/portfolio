f54eac1fb216affd128c0a174bd6af66cf6e7a84
Vlad613 <55757119+Vlad613@users.noreply.github.com>
2020-07-09 15:02:46 +0300

    2.10

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import styles from './Main.module.css';
+import styles from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
+import MainPhotoTest from '../assets/image/MainPhotoTest.jpg'
+
 
 
 let Main = () => {
+
+    const MainPhoto = {
+        backgroundImage: `url(${MainPhotoTest})`,
+    };
+
     return (
         <div className={styles.mainBlock}>
-            <div className={`${styleContainer.container} ${styles.mainContainer}`}>
+            <div className={styles.mainBlockPhoto} style={MainPhoto}>
+                <div className={`${styleContainer.container} ${styles.mainContainer}`}>
 
-                <div className={styles.text}>
-                    <span>HELLO, I AM</span>
-                    <h1>Vlad Zharnikov</h1>
-                    <p>I am Front-end Developer</p>
+                    <div className={styles.text}>
+                        <span>HELLO, I AM</span>
+                        <h1>Vlad Zharnikov</h1>
+                        <p>I am Front-end Developer</p>
+                    </div>
 
-    </div>
-
+                </div>
             </div>
-
         </div>
     );
 }
