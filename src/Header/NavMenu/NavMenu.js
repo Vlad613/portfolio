b4fb5f74be4bc183b22fe548caaefc9e5a1be9f6
Vlad613 <55757119+Vlad613@users.noreply.github.com>
2020-07-10 18:21:00 +0300

    2.12

--- src/Header/NavMenu/NavMenu.js
+++ src/Header/NavMenu/NavMenu.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './NavMenu.module.scss';
 
 
-
 const NavMenu = () => {
-    let navLinkOnFocus = "styles.linkOnFocus";
-    let navLinkOffFocus = "styles.link";
 
+    let [onFocusMode, setFocusMode] = useState(false);
+
+    const activateFocusMode = () => {
+        setFocusMode(true);
+    };
+    const deActivateFocusMode = () => {
+        setFocusMode(false);
+    };
 
     return (
         <div className={styles.navMenu}>
             <ul className={styles.navBarNav}>
-                <li><a href=''
 
+                {!onFocusMode &&
+                <li onMouseOver={activateFocusMode}>
+                    <a href=''
+                       className={styles.link}>MAIN</a></li>}
 
-                >MAIN</a></li>
-                <li><a href='' className={styles.link}>SKILLS</a></li>
-                <li><a href='' className={styles.link}>PROJECTS</a></li>
-                <li><a href='' className={styles.link}>CONTACTS</a></li>
+                {onFocusMode &&
+                <li onMouseOut={deActivateFocusMode}>
+                    <a href=''
+                       className={styles.linkOnFocus}>MAIN</a></li>}
+
+                {/*{!onFocusMode &&*/}
+                {/*<li ><a href=''*/}
+                {/*        className={styles.link}>SKILLS</a></li>}*/}
+                {/*{onFocusMode &&*/}
+                {/*<li><a href=''*/}
+                {/*       className={styles.linkOnFocus}>SKILLS</a></li>}*/}
+                {/*{!onFocusMode &&*/}
+                {/*<li ><a href=''*/}
+                {/*        className={styles.link}>PROJECTS</a></li>}*/}
+                {/*{onFocusMode &&*/}
+                {/*<li><a href=''*/}
+                {/*       className={styles.linkOnFocus}>PROJECTS</a></li>}*/}
+
+                {/*{!onFocusMode &&*/}
+                {/*<li ><a href=''*/}
+                {/*        className={styles.link}>CONTACTS</a></li>}*/}
+                {/*{onFocusMode &&*/}
+                {/*<li><a href=''*/}
+                {/*       className={styles.linkOnFocus}>CONTACTS</a></li>}*/}
+
             </ul>
         </div>
     );
