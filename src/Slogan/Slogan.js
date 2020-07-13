de3124c63eb5e073535aab26708ff10f7b31ab0f
Vlad613 <55757119+Vlad613@users.noreply.github.com>
2020-07-13 17:53:52 +0300

    2.19

--- src/Slogan/Slogan.js
+++ src/Slogan/Slogan.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import styles from './Slogan.module.css';
+import styles from './Slogan.module.scss';
 import styleContainer from "../common/styles/Container.module.css";
+import freelanceJob from '../assets/image/freelanceJob.jpg'
 
 
 function Slogan() {
-  return (
-      <div className={styles.sloganBlock}>
-        <div className={`${styleContainer.container} ${styles.sloganContainer}`}>
-          <h3 className={styles.sloganText}>
-            Рассматриваю варианты удаленной работы
-          </h3>
-          <div>
-            <button>Нанять меня</button>
-          </div>
+
+    const FreelanceJob = {
+        backgroundImage: `url(${freelanceJob})`,
+    };
+
+
+    return (
+        <div className={styles.sloganBlock} style={FreelanceJob}>
+            <div className={`${styleContainer.container} ${styles.sloganContainer}`}>
+                <div className={styles.sloganRow}>
+                    <h2 className={styles.sloganText}>
+                        LET'S WORK TOGETHER!
+                    </h2>
+                    <div>
+                        <a href={''} className={styles.sloganBtn}>HIRE ME!</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default Slogan;
