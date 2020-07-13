6d0d0296aa0c3ec293918271a1d84b179ca1e632
Vlad613 <55757119+Vlad613@users.noreply.github.com>
2020-07-13 18:51:36 +0300

    2.21

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -11,35 +11,10 @@ function Contacts() {
                 <Title text='Contacts' titleSpan='SAY HELLO'/>
 
                 <form className={styles.contactsForm}>
-
-                    <h5 className={styles.contactsFormHead}>Contact Form</h5>
-
-                    <div className={styles.contactsFormRow}>
-                        <div className={styles.contactsFormInputContainer}>
-                            <div className={styles.contactsFormInput}>
-                                <input type="text" placeholder={"Your Name"}/>
-                            </div>
-                        </div>
-                        <div className={styles.contactsFormInputContainer}>
-                            <div className={styles.contactsFormInput}>
-                                <input type="text" placeholder={"Your Email"}/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.contactsFormRow}>
-                        <div className={styles.contactsFormTextContainer}>
-                            <div className={styles.contactsFormText}>
-                                <textarea placeholder={"Your message..."}/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.contactsFormRow}>
-                        <div className={styles.contactsFormButtonContainer}>
-                        <button type="submit" className={styles.submitBtn}>SEND</button>
-                    </div>
-                    </div>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea/>
+                    <button type="submit" className={styles.submitBtn}>Отправить</button>
                 </form>
             </div>
         </div>
@@ -47,3 +22,4 @@ function Contacts() {
 }
 
 export default Contacts;
+
