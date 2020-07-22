7e9032d63099e7f7422c55a0fe39d97274c4032a
Vlad613 <55757119+Vlad613@users.noreply.github.com>
2020-07-22 16:34:16 +0300

    3.62

--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -4,12 +4,13 @@ const initialState = {appWindowSize: window.innerWidth};
 
 
 const appReducer = (state = initialState, action) => {
-
+debugger
     switch (action.type) {
         case RESIZE_HEADER:
             return {
-                ...state,
-                appWindowSize: action.windowSize
+                // ...state,
+                // appWindowSize: action.windowSize
+               ...state, appWindowSize: action.windowSize
             };
         default:
             return state;
